a61a8ead9d7ab846fd3edbb3549bd747647c8879
Bishwa Kiran Poudel <bishwakiran725@gmail.com>
2023-10-06 14:07:13 +0545

    Added app colors

---
+++ frontend/lib/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF00BF63);
+  static const Color accentColor = Color.fromARGB(255, 219, 239, 222);
+  static const Color secondarytextColor = Color.fromARGB(255, 255, 255, 255);
+  static const Color textColor = Color(0xFF3B4A3F);
+  static const Color textaccentColor = Color(0xFF9EB0A2);
+  static const Color backgroundColor = Color.fromARGB(255, 255, 255, 255);
+}
