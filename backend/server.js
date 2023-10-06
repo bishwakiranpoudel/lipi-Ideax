8ec033b1f04060a4dd341b21472158f2a1b41ce1
Bishwa Kiran Poudel <bishwakiran725@gmail.com>
2023-10-06 13:07:20 +0545

    Initial configuration

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const admin = require('firebase-admin');
+const multer = require('multer');
+const app = express();
+const port = 3000;
+
+// Initialize Firebase Admin SDK
+const serviceAccount = require('./serviceAccountKey.json'); 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: 'gs://lipi-b8642.appspot.com',
+});
+
+//Firebase storage
+const bucket = admin.storage().bucket();
+const db = admin.firestore(); // Initialize Firestore
+
+
+
+
+
+// Multer storage configuration for file uploads
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.use(express.json());
+
+// Define routes
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
