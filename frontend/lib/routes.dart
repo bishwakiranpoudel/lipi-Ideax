7df33be542a9729ab1324284e081b945dfa17a8b
Bishwa Kiran Poudel <bishwakiran725@gmail.com>
2023-10-06 15:18:21 +0545

    Added wrapper signup sign in and home page routes

--- frontend/lib/routes.dart
+++ frontend/lib/routes.dart
@@ -1,3 +1,14 @@
 import 'package:flutter/widgets.dart';
+import 'package:frontend/screens/addprofile.dart';
+import 'package:frontend/screens/home.dart';
+import 'package:frontend/screens/login.dart';
+import 'package:frontend/screens/signup.dart';
+import 'package:frontend/screens/wrapper.dart';
 
-final Map<String, WidgetBuilder> routes = {};
+final Map<String, WidgetBuilder> routes = {
+  '/': (context) => Wrapper(),
+  '/login': (context) => LoginScreen(),
+  '/signup': (context) => SignupScreen(),
+  '/home': (context) => HomeScreen(),
+  '/addprofile': (context) => AddProfileScreen(),
+};
